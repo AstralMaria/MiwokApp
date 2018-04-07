7f0a8e3fe7fcce469884c7c1309d7c7f0546bbf9
Astral Maria <rsteer67@gmail.com>
2018-04-07 19:06:18 +0100

    no message

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,34 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+
+        ArrayList<String> numbers = new ArrayList<String>();
+        numbers.add("One");
+        numbers.add("Two");
+        numbers.add("Three");
+        numbers.add("Four");
+        numbers.add("Five");
+        numbers.add("Six");
+        numbers.add("Seven");
+        numbers.add("Eight");
+        numbers.add("Nine");
+        numbers.add("Ten");
+
+
+            LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        int index = 0;
+        while (index <numbers.size());
+        {
+            TextView wordView = new TextView(this);
+            wordView.setText(numbers.get(index));
+            rootView.addView(wordView);
+            index ++;
+
+        }
     }
+
 }
+
+
+
